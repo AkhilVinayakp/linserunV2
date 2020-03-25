bc94992e540b0247d5084658d7768be7641305e5
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-03-25 12:31:37 +0530

    angular form events added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,13 +13,20 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void { }
 
-
+  cmd_ser_text:any;
+  action_selected="RUN";
   close_window(){
     this.electron_window.window.close();
   }
   min_window()
   {
     this.electron_window.window.minimize();
+  }
+  action_changed(){
+    console.log(this.action_selected);
+  }
+  text_changed(){
+    console.log(this.cmd_ser_text);
   }
 
 }
