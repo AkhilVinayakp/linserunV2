d8b5c28ca2fbe7b14de2cb9ffb6726a5bf89566e
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-03-25 13:11:08 +0530

    child processto run the commands

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../core/services/electron/electron.service';
 //importing data serives
 import { DataService } from '../data.service';
+const { spawn } = require("child_process");
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -27,6 +28,11 @@ export class HomeComponent implements OnInit {
   cmd_ser_text:any;
   placeh_text:any;
   action_selected="RUN";
+  data_api:Object={
+    out_flag:1,
+    out_stream:null,
+
+  }
   close_window(){
     this.electron_window.window.close();
   }
@@ -41,6 +47,35 @@ export class HomeComponent implements OnInit {
   }
   text_changed(){
     console.log(this.cmd_ser_text);
+    if(this.action_selected=="RUN")
+      this.run_command();
+    else
+      this.search_text();
+
+
+  }
+  run_command()
+  {
+    let out_data = spawn(this.cmd_ser_text);
+    out_data.stdout.on('data',(data)=>{this.run_command_stdout(data)})
+    out_data.stderr.on('data',(data)=>{this.run_command_stderr(data)})
+    out_data.on('data',(data)=>{this.run_command_on(data)})
+  }
+
+  run_command_stdout(data:any){
+      console.log(data.toString());
+  }
+  run_command_stderr(data:any){
+    console.log(data.toString());
+  }
+  run_command_on(data:any){
+    console.log(data.toString());
+  }
+
+
+  search_text()
+  {
+
   }
 
 }
